4a64b14b42c65258e2fdd4166a1a84485656053f
schoolonlyaccount <macman5@tkf7.net>
2026-09-08 17:32:35 -0500

    Final Changes for week 01

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const mongodb = require('../data/database');
+const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const mongodb = require('./db/connect');
 
-const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
