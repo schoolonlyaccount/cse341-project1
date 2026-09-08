a2d58e5ca705176aac4b4f76b0d4a699c9b45d95
schoolonlyaccount <macman5@tkf7.net>
2026-09-08 17:10:07 -0500

    Changed "users" to "contacts"

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World'); });
 
-router.use('/users', require('./users'));
+router.use('/users', require('./contacts'));
 
 module.exports = router;
