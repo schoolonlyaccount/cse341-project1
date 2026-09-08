00642e4af07115f0ba700dd9c562dbf98f84854c
schoolonlyaccount <macman5@tkf7.net>
2026-09-08 17:11:56 -0500

    Minor tweak

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World'); });
 
-router.use('/users', require('./contacts'));
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
